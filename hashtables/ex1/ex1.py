a37f59b059c8022d4994e0a90aa18f4c9c3c23a0
emilme <emilme@sir.mle>
2020-10-02 09:35:37 -0700

    ex1 working, run time needs improvement

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,24 @@
+cache = {}
+
 def get_indices_of_item_weights(weights, length, limit):
     """
-    YOUR CODE HERE
-    """
-    # Your code here
+    Find two items in the weights list that add to limit.
 
-    return None
+    ...Loop through the length?
+
+    Return  a tuple from containg numbers from largest to smallest.
+
+    If none return none
+
+    ...add all nums with first then add to cache and so on?
+    """
+    if length > 1:
+        for i in range(0, length):
+            for x in range(0, length):
+                if i != x:
+                    if weights[i] + weights[x] == limit:
+                        if x < i:
+                            return (i, x)
+                        return (x , i)
+    else:
+        return None
